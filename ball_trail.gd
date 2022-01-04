0962b8b05110f00d1bfbfb1593270815cafb6d21
Oliver Gilliland <omgilliland@gmail.com>
2022-01-04 13:58:23 +0000

    Improved ball trail

--- ball_trail.gd
+++ ball_trail.gd
@@ -1,18 +1,26 @@
 extends Line2D
 
-var num_points: int = 64
+const MAX_POINTS: int = 64
+
+var num_points: int
 
 func get_ball_pos() -> Vector2:
 	return get_node("../../Ball").global_position
 
+func get_speed() -> float:
+	return get_node("../../Ball").vel_speed
+
 func init() -> void:
 	clear_points()
-	for i in range(num_points):
-		add_point(get_ball_pos())
+#	for i in range(MIN_POINTS):
+#		add_point(get_ball_pos())
 
 func _ready() -> void:
 	init()
 
 func _physics_process(delta: float) -> void:
+	num_points = MAX_POINTS * clamp(get_speed() * 0.001, 0, 1)
+	
 	add_point(get_ball_pos(), 0)
-	remove_point(num_points)
+	while(get_point_count() > num_points):
+		remove_point(num_points)
