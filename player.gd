1ce0c924ad37290b4b9657c1016a42d1f3703086
Oliver Gilliland <omgilliland@gmail.com>
2022-01-04 12:38:07 +0000

    Hide mouse cursor

--- player.gd
+++ player.gd
@@ -1,5 +1,16 @@
 extends "res://paddle.gd"
 
-func _input(event):
-	if event is InputEventMouseMotion:
-		target_height = event.position.y
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent) -> void:
+	# capture / free cursor
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	# move paddle
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		target_height += event.relative.y
